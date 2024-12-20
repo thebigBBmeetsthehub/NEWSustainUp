a60828d391351a9a2e16761397eced5f7a171b8a
Briar <briarlynnbryant@gmail.com>
2024-12-19 19:59:20 -0800

    Initial Commit

---
+++ NEWSustainUp/NEWSustainUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  NEWSustainUpApp.swift
+//  NEWSustainUp
+//
+//  Created by Briar Bryant on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct NEWSustainUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NEWSustainUpTests/NEWSustainUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  NEWSustainUpTests.swift
+//  NEWSustainUpTests
+//
+//  Created by Briar Bryant on 12/19/24.
+//
+
+import Testing
+@testable import NEWSustainUp
+
+struct NEWSustainUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
